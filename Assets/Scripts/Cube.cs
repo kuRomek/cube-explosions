22f4d186a3920f981f8e41e70b6d28804ebb6df3
kuRomek <qustes@gmail.com>
2024-05-12 16:27:38 +0400

    delegate spliting cubes to Cube class

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,23 +1,68 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
     private float _splitChance;
-
-    public float SplitChance => _splitChance;
+    private float _splitChanceDivider;
+    private float _cubeScaleDivider;
 
     private void Awake()
     {
         _splitChance = 1f;
-        float minColorIntensity = 0f;
-        float maxColorIntensity = 1f;
-        GetComponent<Renderer>().material.color = new Color(Random.Range(minColorIntensity, maxColorIntensity),
-                                                            Random.Range(minColorIntensity, maxColorIntensity),
-                                                            Random.Range(minColorIntensity, maxColorIntensity));
+        _splitChanceDivider = 2f;
+        _cubeScaleDivider = 2f;
+        GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value);
     }
 
-    public void SetSplitChance(float previousCubeChance, float chanceToSplitDivider)
+    public List<Cube> Split()
     {
-        _splitChance = previousCubeChance / chanceToSplitDivider;
+        if (Random.value < _splitChance)
+        {    
+            int minNewCubesCount = 2;
+            int maxNewCubesCount = 6;
+            int newCubesCount = Random.Range(minNewCubesCount, maxNewCubesCount + 1);
+
+            List<Cube> newCubes = new List<Cube>();
+
+            for (int i = 0; i < newCubesCount; i++)
+            {
+                float leftRandomOffset = -0.5f;
+                float rightRandomOffset = 0.5f;
+                float yAxisLift = 0.3f;
+                Vector3 randomOffset = GetRandomVector3(leftRandomOffset, rightRandomOffset);
+                Vector3 newCubePosition = transform.position + new Vector3(randomOffset.x, randomOffset.y + yAxisLift, randomOffset.z);
+
+                float minRandomAngle = -45f;
+                float maxRandomAngle = 45f;
+                Quaternion newCubeRotation = Quaternion.Euler(GetRandomVector3(minRandomAngle, maxRandomAngle));
+
+                Cube newCube = Instantiate(gameObject.GetComponent<Cube>(), newCubePosition, newCubeRotation);
+                newCube.transform.localScale /= _cubeScaleDivider;
+                newCube.SetSplitChance(_splitChance);
+
+                newCubes.Add(newCube);
+            }
+
+            return newCubes;
+        }
+        else
+        {
+            return new List<Cube>();
+        }
+    }
+
+    private Vector3 GetRandomVector3(float minValue, float maxValue)
+    {
+        float randomXValue = Random.Range(minValue, maxValue);
+        float randomYValue = Random.Range(minValue, maxValue);
+        float randomZValue = Random.Range(minValue, maxValue);
+        
+        return new Vector3(randomXValue, randomYValue, randomZValue);
+    }
+
+    public void SetSplitChance(float previousSplitChance)
+    {
+        _splitChance = previousSplitChance / _splitChanceDivider;
     }
 }
